d8f2ccb2983dc6b555eaa1c427101535d67439d1
Aleksander <asava@sibmail.com>
2023-08-25 19:22:38 +0700

    ok

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,13 @@
 'use client'
-
 import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 import Link from 'next/link'
-import { GiHamburgerMenu } from 'react-icons/gi'
 import { RxCross1, RxHamburgerMenu } from 'react-icons/rx'
 import { menuItems } from '@/constants'
 import Image from 'next/image'
 
 const Navbar = () => {
   const pathname = usePathname()
-
-  // className={
-  //   'text-white p-2 ' +
-  //   (pathname === item.path
-  //     ? 'bg-white text-black rounded-lg text-gray-900'
-  //     : '')
-  // }
 
   const [isClick, setIsClick] = useState(false)
   const toggleNavbar = () => {
@@ -90,30 +81,6 @@ const Navbar = () => {
                   {item.name}
                 </Link>
               ))}
-              {/* <a
-                href="/"
-                className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Home
-              </a>
-              <a
-                href="/"
-                className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                About
-              </a>
-              <a
-                href="/"
-                className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Services
-              </a>
-              <a
-                href="/"
-                className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-              >
-                Contact
-              </a> */}
             </div>
           </div>
         )}
@@ -123,128 +90,3 @@ const Navbar = () => {
 }
 
 export default Navbar
-
-// 'use client'
-// import { useState } from 'react'
-
-// const Navbar = () => {
-//   const [isClick, setIsClick] = useState(false)
-//   const toggleNavbar = () => {
-//     setIsClick(!isClick)
-//   }
-//   return (
-//     <>
-//       <nav className=" bg-black">
-//         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-//           <div className="flex items-center justify-between h-16">
-//             <div className="flex items-center">
-//               <div className="flex-shrink-0">
-//                 <a href="/" className=" text-white">
-//                   logo
-//                 </a>
-//               </div>
-//             </div>
-//             <div className="hidden md:block">
-//               <div className="ml-4 flex items-center space-x-4">
-//                 <a
-//                   href="/"
-//                   className=" text-white hover:bg-white hover:text-black rounded-lg p-2"
-//                 >
-//                   Home
-//                 </a>
-//                 <a
-//                   href="/"
-//                   className=" text-white hover:bg-white hover:text-black rounded-lg p-2"
-//                 >
-//                   About
-//                 </a>
-//                 <a
-//                   href="/"
-//                   className=" text-white hover:bg-white hover:text-black rounded-lg p-2"
-//                 >
-//                   Services
-//                 </a>
-//                 <a
-//                   href="/"
-//                   className=" text-white hover:bg-white hover:text-black rounded-lg p-2"
-//                 >
-//                   Contact
-//                 </a>
-//               </div>
-//             </div>
-//             <div className=" md:hidden flex items-center">
-//               <button
-//                 className=" inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-//                 onClick={toggleNavbar}
-//               >
-//                 {isClick ? (
-//                   <svg
-//                     className="h-6 w-6"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                     fill="none"
-//                     viewBox="0 0 24 24"
-//                     stroke="currentColor"
-//                   >
-//                     <path
-//                       strokeLinecap="round"
-//                       strokeLinejoin="round"
-//                       strokeWidth={2}
-//                       d="M6 18L18 6M6 6l12 12"
-//                     />
-//                   </svg>
-//                 ) : (
-//                   <svg
-//                     className="h-6 w-6"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                     fill="none"
-//                     viewBox="0 0 24 24"
-//                     stroke="currentColor"
-//                   >
-//                     <path
-//                       strokeLinecap="round"
-//                       strokeLinejoin="round"
-//                       strokeWidth={2}
-//                       d="M4 6h16M4 12h16m-7 6h7"
-//                     />
-//                   </svg>
-//                 )}
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//         {isClick && (
-//           <div className="md:hidden">
-//             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-//               <a
-//                 href="/"
-//                 className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-//               >
-//                 Home
-//               </a>
-//               <a
-//                 href="/"
-//                 className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-//               >
-//                 About
-//               </a>
-//               <a
-//                 href="/"
-//                 className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-//               >
-//                 Services
-//               </a>
-//               <a
-//                 href="/"
-//                 className=" text-white block hover:bg-white hover:text-black rounded-lg p-2"
-//               >
-//                 Contact
-//               </a>
-//             </div>
-//           </div>
-//         )}
-//       </nav>
-//     </>
-//   )
-// }
-
-// export default Navbar
