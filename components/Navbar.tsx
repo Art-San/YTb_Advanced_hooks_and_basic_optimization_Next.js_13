a58ed29d4f37134fcbc0dd3f2e6928f40bca1248
Aleksander <asava@sibmail.com>
2023-08-25 22:31:19 +0700

    ok

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                     className={
                       'text-white text-[15px] p-2 ' +
                       (pathname === item.path
-                        ? 'bg-white text-black rounded-lg'
+                        ? 'bg-white text-slate-800 rounded-lg'
                         : '')
                     }
                   >
@@ -74,7 +74,7 @@ const Navbar = () => {
                   className={
                     'text-white block p-2 ' +
                     (pathname === item.path
-                      ? 'bg-white text-black rounded-lg'
+                      ? 'bg-white text-slate-800 rounded-lg'
                       : '')
                   }
                 >
